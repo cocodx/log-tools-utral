5b19d2c8aa6b2c0b9b6ff01b45a52e2bffe4af0c
刘刚 <2cevening@gmail.com>
2022-07-06 21:06:35 +0800

    feature(*):first commit

---
+++ log4j-tools/src/main/java/io/github/cocodex/log4j/Test.java
@@ -0,0 +1,16 @@
+package io.github.cocodex.log4j;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author amazfit
+ * @date 2022-07-06 上午1:49
+ **/
+public class Test {
+
+    private static final Logger logger = Logger.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("xxxxxxxxxxxx");
+    }
+}
